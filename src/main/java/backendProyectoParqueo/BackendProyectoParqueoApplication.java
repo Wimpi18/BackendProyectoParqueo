7dd8f8c4fa3b945f8b8c96df8a128ecc8bb26dab
Winsor <winsoromar12@gmail.com>
2025-05-04 21:42:36 -0400

    init: configuracion inicial del proyecto

---
+++ src/main/java/backendProyectoParqueo/BackendProyectoParqueoApplication.java
@@ -0,0 +1,13 @@
+package backendProyectoParqueo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProyectoParqueoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProyectoParqueoApplication.class, args);
+	}
+
+}
