dac786ccfa8fc108c6e4c4d402631265f6152e47
Winsor <winsoromar12@gmail.com>
2025-05-19 16:03:10 -0400

    feat: add standard api response

--- src/main/java/backendProyectoParqueo/controller/PagoParqueoController.java
+++ src/main/java/backendProyectoParqueo/controller/PagoParqueoController.java
@@ -3,14 +3,17 @@ package backendProyectoParqueo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import backendProyectoParqueo.dto.ApiResponse;
 import backendProyectoParqueo.model.PagoParqueo;
 import backendProyectoParqueo.service.PagoParqueoService;
+import backendProyectoParqueo.util.ApiResponseUtil;
 
 @RestController
 @RequestMapping("/api/pago-parqueo")
@@ -24,11 +27,11 @@ public class PagoParqueoController {
     }
 
     @GetMapping
-    public List<PagoParqueo> getAllPagoParqueos() {
-        return pagoParqueoService.findAll();
+    public ResponseEntity<ApiResponse<List<PagoParqueo>>> getAllPagoParqueos() {
+        return ApiResponseUtil.success("Todos los pagos realizados al parqueo", pagoParqueoService.findAll());
     }
 
-    @PostMapping("/crear")
+    @PostMapping()
     public int createPagoParqueo(@RequestBody PagoParqueo pagoParqueo) {
         return 0;
     }
