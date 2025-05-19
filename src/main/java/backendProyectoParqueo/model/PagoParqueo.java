f3c5af0e9ec08734ecd2d9bceff312a67447dbe8
Winsor <winsoromar12@gmail.com>
2025-05-19 17:07:44 -0400

    feat: add simple validators in pago parqueo

--- src/main/java/backendProyectoParqueo/model/PagoParqueo.java
+++ src/main/java/backendProyectoParqueo/model/PagoParqueo.java
@@ -2,6 +2,9 @@ package backendProyectoParqueo.model;
 
 import java.sql.Date;
 import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
@@ -9,7 +12,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 
 @Entity()
 @Table(name = "pago_parqueo")
@@ -27,11 +33,13 @@ public class PagoParqueo {
     @Column(name = "idCajero", columnDefinition = "UUID")
     private UUID idCajero;
 
-    @Column(name = "montoPagado", columnDefinition = "money", nullable = false)
+    @Column(name = "montoPagado", columnDefinition = "numeric", nullable = false)
+    @Min(value = 1, message = "El monto a pagar debe ser mayor o igual a 1 Bs.")
+    @Max(value = 1000, message = "El monto a pagar debe ser menor o igual a 1000 Bs.")
     private double montoPagado;
 
     @Column(name = "fechaHoraPago", columnDefinition = "timestamp without time zone", nullable = false)
-    private Timestamp fechaHoraPago;
+    private Timestamp fechaHoraPago = Timestamp.from(Instant.now());
 
     @Column(nullable = false)
     private Date[] meses;
@@ -39,6 +47,17 @@ public class PagoParqueo {
     @Column(name = "nroEspacioPagado", columnDefinition = "smallint")
     private int nroEspacioPagado;
 
+    // Métodos de persistencia
+    @PrePersist
+    protected void onCreate() {
+        if (this.fechaHoraPago == null) {
+            ZoneId zonaBolivia = ZoneId.of("America/La_Paz");
+            ZonedDateTime fechaHoraBolivia = ZonedDateTime.now(zonaBolivia);
+            this.fechaHoraPago = Timestamp.valueOf(fechaHoraBolivia.toLocalDateTime());
+        }
+    }
+
+    // Métodos getters y setters
     public Long getId() {
         return id;
     }
