ce5e12a91c09f749d726b4ceb2afe5ffe765c027
Winsor <winsoromar12@gmail.com>
2025-05-19 12:47:13 -0400

    fix: change order imports

--- src/main/java/backendProyectoParqueo/model/Cliente.java
+++ src/main/java/backendProyectoParqueo/model/Cliente.java
@@ -1,7 +1,14 @@
 package backendProyectoParqueo.model;
 
-import jakarta.persistence.*;
 import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "cliente")
