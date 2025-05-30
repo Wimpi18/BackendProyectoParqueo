eddc8a01b2ad12de20f0b4341d8958ef884e6f26
Winsor <winsoromar12@gmail.com>
2025-05-30 00:08:04 -0400

    feat(TarifaController): add tarifa vigente

--- src/main/java/backendProyectoParqueo/controller/TarifaController.java
+++ src/main/java/backendProyectoParqueo/controller/TarifaController.java
@@ -3,11 +3,19 @@ package backendProyectoParqueo.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import backendProyectoParqueo.dto.ApiResponse;
 import backendProyectoParqueo.dto.TarifaDTO;
-
+import backendProyectoParqueo.enums.TipoVehiculo;
+import backendProyectoParqueo.model.Tarifa;
 import backendProyectoParqueo.service.TarifaService;
+import backendProyectoParqueo.util.ApiResponseUtil;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -21,6 +29,17 @@ public class TarifaController {
     public ResponseEntity<List<TarifaDTO>> listarTarifas() {
         List<TarifaDTO> tarifas = tarifaService.listarTarifas();
         return ResponseEntity.ok(tarifas);
+    }
+
+    @GetMapping("/vigente")
+    public ResponseEntity<ApiResponse<Tarifa>> findTarifaVigente(
+            @RequestParam(required = true) String tipoCliente,
+            @RequestParam(required = true) TipoVehiculo tipoVehiculo) {
+        String message = "Última tarifa vigente para " + tipoCliente + " y "
+                + tipoVehiculo;
+        return ApiResponseUtil.success(message,
+                tarifaService.findTarifaByTipoClienteYVehiculo(tipoCliente,
+                        tipoVehiculo));
     }
 
     // Crear una nueva tarifa
