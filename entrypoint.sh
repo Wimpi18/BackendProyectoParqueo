d86ff1bc48cd70e8b504f47ac8bc31a1e27188a9
Winsor <winsoromar12@gmail.com>
2025-06-11 17:40:45 -0400

    fix: entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-
 echo "✅ Generando keystore..."
+
 keytool -genkey \
   -alias "jwt-sign-key" \
   -keyalg RSA \
   -keysize 4096 \
-  -keystore "src/main/resources/jwt-keystore.jks" \
-  -validity 3650 \
+  -keystore "./src/main/resources/jwt-keystore.jks" \
   -storepass "aladinos" \
   -keypass "aladinos" \
   -dname "CN=JWT, OU=Dev, O=MyCompany, L=LaPaz, ST=LaPaz, C=BO"
 
 echo "🚀 Iniciando la aplicación..."
-exec java -jar app.jar
