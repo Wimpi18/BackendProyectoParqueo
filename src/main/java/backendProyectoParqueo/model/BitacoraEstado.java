9d4b46da5f44f1a8a65b20960eb04eee9849ceee
NahuelTG <nahueltorrezg@gmail.com>
2025-06-11 22:14:20 -0400

    update: bitacora estado

--- src/main/java/backendProyectoParqueo/model/BitacoraEstado.java
+++ src/main/java/backendProyectoParqueo/model/BitacoraEstado.java
@@ -1,9 +1,14 @@
 package backendProyectoParqueo.model;
 
 
+import java.time.LocalDateTime;
+
 import backendProyectoParqueo.enums.TipoAccion;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,23 +19,37 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "bitacora_estado")
 public class BitacoraEstado {
-  @Id
-  @GeneratedValue (strategy = GenerationType.IDENTITY)
-  @Column(name = "id", unique = true, updatable = false)
-  private Long id;
-  
-  @ManyToOne
-  @JoinColumn(name = "idUsuario", referencedColumnName = "id", nullable = true)
-  private Usuario idUsuario;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_usuario", referencedColumnName = "id") // FK al Usuario base
+    private Usuario usuarioAfectado;
 
-  @ManyToOne
-  @JoinColumn(name = "idAdministrador" , referencedColumnName = "id", nullable = true)
-  private Administrador idAdministrador;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_administrador", referencedColumnName = "id") // FK al Administrador que ejecuta
+    private Administrador adminEjecutor;
 
-  @Column (name = "tipo_accion")
-  private TipoAccion tipo;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo_accion", nullable = false)
+    private TipoAccion tipoAccion;
 
-  @Column (name = "motivo")
-  private String motivo;
+    @Column(name = "motivo", length = 500, nullable = false)
+    private String motivo;
+
+    @Column(name = "fecha_hora_cambio", nullable = false)
+    private LocalDateTime fechaHoraCambio;
+
+    // Constructores, Getters, Setters
+    public BitacoraEstado() {
+        this.fechaHoraCambio = LocalDateTime.now();
+    }
+     public BitacoraEstado(Usuario usuarioAfectado, Administrador adminEjecutor, TipoAccion tipoAccion, String motivo) {
+        this();
+        this.usuarioAfectado = usuarioAfectado;
+        this.adminEjecutor = adminEjecutor;
+        this.tipoAccion = tipoAccion;
+        this.motivo = motivo;
+    }
 }
