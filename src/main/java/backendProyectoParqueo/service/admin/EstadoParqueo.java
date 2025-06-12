4b5935e8ffd9694fccfd670e269dcc073799e5e9
NahuelTG <nahueltorrezg@gmail.com>
2025-06-11 22:35:45 -0400

    add: bitacora estado repository

---
+++ src/main/java/backendProyectoParqueo/service/admin/EstadoParqueo.java
@@ -0,0 +1,11 @@
+package backendProyectoParqueo.service.admin;
+
+import org.springframework.stereotype.Service;
+
+import backendProyectoParqueo.repository.AdministradorRepository;
+import backendProyectoParqueo.repository.ParqueoRepository;
+
+@Service
+public class EstadoParqueo {
+
+}
